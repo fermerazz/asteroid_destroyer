8f1dc1e0edee7664a1a2d4e2aee124f49ce56e11
fermerazz <fermerazz12@gmail.com>
2025-04-23 14:52:55 -0600

    Added logic to create a black window that closes propperly

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import pygame
+from constants import *
 
 def __main__():
+    pygame.init()
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
+    #Creating a display screen
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-
+    #Infinite game loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
 
 
 
