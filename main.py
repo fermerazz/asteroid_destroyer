4da78f8aa2b5cca880a266a5a17e72134c1a4d85
fermerazz <fermerazz12@gmail.com>
2025-04-23 18:59:00 -0600

    Player class was added it is a child from the CircleShape class. Now the game draws a triangle in the middle of the screen

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import pygame
 from constants import *
+from player import Player
 
 def __main__():
     pygame.init()
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    Clock = pygame.time.Clock()
+    dt = 0 #Change in frames over last call 
 
     #Creating a display screen
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    player  = Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
 
     #Infinite game loop
     while True:
@@ -16,9 +21,13 @@ def __main__():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        Clock.tick(60)
+        dt = Clock.tick(60)/1000
+        
 
-
+    
 
 
 
